043b9e1c71e6c1f33edf4ce6822eac2b49eb878c
Faryzal2020 <fary290796@gmail.com>
2026-05-02 14:49:06 +0700

    update

--- minecraft/startserver.sh
+++ minecraft/startserver.sh
@@ -110,7 +110,7 @@ case "$GC_TYPE" in
     *) # Default to G1GC
         JVM_OPTS="$JVM_OPTS -XX:+UseG1GC"
         if [ "$USE_AIKAR" = "true" ]; then
-            JVM_OPTS="$JVM_OPTS -XX:+UnlockExperimentalVMOptions -XX:+MaxGCPauseMillis=200 -XX:+UnlockDiagnosticVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1"
+            JVM_OPTS="$JVM_OPTS -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=200 -XX:+UnlockDiagnosticVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1"
         fi
         ;;
 esac
